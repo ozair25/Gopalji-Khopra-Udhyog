1be46984798eb8857dbafc8c15d8229f9696548a
Sayed Ozair Mehmood <sayedozair25@gmail.com>
2026-06-27 20:04:27 +0530

    Refactor routes to include ProtectedRoute and admin paths

--- App.tsx
+++ App.tsx
@@ -2,7 +2,9 @@
  * @license
  * SPDX-License-Identifier: Apache-2.0
  */
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Products from './components/Products';
@@ -13,10 +15,13 @@ import AboutUs from './components/AboutUs';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import WhatsAppButton from './components/WhatsAppButton';
+
 import Login from './pages/Login';
 import AdminDashboard from './pages/AdminDashboard';
-import ProtectedRoute from './components/ProtectedRoute';
 import PrivacyPolicy from './pages/PrivacyPolicy';
+
+import ProtectedRoute from './components/ProtectedRoute';
+import { ADMIN_SECURITY_PATHS } from './config/adminPaths';
 
 function MainSite() {
   return (
@@ -41,16 +46,24 @@ export default function App() {
   return (
     <Router>
       <Routes>
+        {/* Public Routes */}
         <Route path="/" element={<MainSite />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="/login" element={<Login />} />
-        <Route 
-          path="/admin" 
+
+        {/* Secret Login Route */}
+        <Route
+          path={ADMIN_SECURITY_PATHS.LOGIN}
+          element={<Login />}
+        />
+
+        {/* Secret Admin Dashboard */}
+        <Route
+          path={ADMIN_SECURITY_PATHS.DASHBOARD}
           element={
             <ProtectedRoute>
               <AdminDashboard />
             </ProtectedRoute>
-          } 
+          }
         />
       </Routes>
     </Router>
